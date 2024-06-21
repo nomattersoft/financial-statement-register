6a9803b1df02c5bfb8aebe78a4a3128cb4f68461
nomattersoft <nomatter.soft@gmail.com>
2024-06-21 23:59:24 +0300

    Initial commit.

---
+++ src/main/java/space/objectable/financial_statement_register/FinancialStatementRegisterApplication.java
@@ -0,0 +1,13 @@
+package space.objectable.financial_statement_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialStatementRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialStatementRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/objectable/financial_statement_register/FinancialStatementRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package space.objectable.financial_statement_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialStatementRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
